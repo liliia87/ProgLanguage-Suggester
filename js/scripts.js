f78e63afc30f499fe771620e17247c656d55ec5b
liliia87 <lilia.krivelyova@gmail.com>
2022-04-10 00:17:26 -0700

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
     $("form#form1").submit(function(event){
         event.preventDefault();
+        //$("#output").html("");
         const userName = $("#name").val();
         const progExp = $("#purpose").val();
         const selectOfProgLang = $("#purpose1").val();
         const typeOfProgramming = $("input:radio[name=purpose3]:checked").val();
         const hoursPerDay = parseInt($("#hours").val());
-        const x = document.getElementById("volume");
-        const defaultVal = x.defaultValue;
-        const currentVal = x.ariaValueMax;
+        const t = $("#volume").val();
+        let message = "";
+        
+
 
         // if(progExp == "Yes" && selectOfProgLang == "Yes" && typeOfProgramming == "Back end"){
         //    $("#quote").html(". Dear " + userName + " the best language for you is C++"); 
@@ -20,36 +22,49 @@ $(document).ready(function(){
         //     $("#quote").html(". Dear " + userName + " the best language for you is JavaScript"); 
         //  }  
         if (progExp == "Yes"){
-            if (selectOfProgLang == "Yes" && typeOfProgramming == "Back end"){
-                $("#quote").html(". Dear " + userName + ", the best language for you is C++");
-            }
-            if (selectOfProgLang == "No" && typeOfProgramming == "Front end"){
-                $("#quote").html(". Dear " + userName + ", the best language for you is Python, Ruby");
-            }
-            if (selectOfProgLang == "No" && typeOfProgramming == "Back end"){
-                $("#quote").html(". Dear " + userName + ", the best language for you is Python, C++");
-            }
-            if (selectOfProgLang == "Yes" && typeOfProgramming == "Front end"){
-                $("#quote").html(". Dear " + userName + ", the best language for you is Ruby, C++");
-            }
-        } else{
-            if(hoursPerDay<3){
-                $("#quote").html(". Dear " + userName + ", the better way to star is reading!");
-            } else{
+            if(hoursPerDay < 3){
+                message = "Dear " + userName + ", the best language for you is Python. With "+t+"% of success";
+            } else{ 
                 if (selectOfProgLang == "Yes" && typeOfProgramming == "Back end"){
-                    $("#quote").html(". Dear " + userName + ", the best language for you is JavaScript");
+                    message = "Dear " + userName + ", the best language for you is C++. With "+t+"% of success";
                 }
                 if (selectOfProgLang == "No" && typeOfProgramming == "Front end"){
-                    $("#quote").html(". Dear " + userName + ", the best language for you is JavaScript");
+                    message = "Dear " + userName + ", the best language for you is Ruby. With "+t+"% of success";
                 }
                 if (selectOfProgLang == "No" && typeOfProgramming == "Back end"){
-                    $("#quote").html(". Dear " + userName + ", the best language for you is JavaScript");
+                    message = "Dear " + userName + ", the best language for you to start is Python. With "+t+"% of success";
                 }
                 if (selectOfProgLang == "Yes" && typeOfProgramming == "Front end"){
-                    $("#quote").html(". Dear " + userName + ", the best language for you is JavaScript");
+                    message = "Dear " + userName + ", the best language for you is JavaScript. With "+t+"% of success";
+                }
+            }
+        } else{
+            if(hoursPerDay < 3){
+                message = "Dear " + userName + ", the best language for you is Python. With "+t+"% of success";
+            } else{
+                if (selectOfProgLang == "Yes" && typeOfProgramming == "Back end"){
+                    message = "Dear " + userName + ", the best language for you is C++. With "+t+"% of success";
+                }
+                if (selectOfProgLang == "No" && typeOfProgramming == "Front end"){
+                    message = "Dear " + userName + ", the best language for you is JavaScript. With "+t+"% of success";
+                }
+                if (selectOfProgLang == "No" && typeOfProgramming == "Back end"){
+                    message = "Dear " + userName + ", the best language for you is Python. With "+t+"% of success";
+                }
+                if (selectOfProgLang == "Yes" && typeOfProgramming == "Front end"){
+                    message = "Dear " + userName + ", the best language for you is JavaScript. With "+t+"% of success";
                 }
             }
         }
+
+        $("#output").fadeOut("slow", function() {
+            $("#output").html(message);
+        });
+
+        $("#output").fadeIn('slow');
+         
     });
+
+
 
 });
