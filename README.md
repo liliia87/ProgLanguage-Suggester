3792d38e4e3972b047a431f300c21884cc95301d
liliia87 <lilia.krivelyova@gmail.com>
2022-04-16 17:24:08 -0700

    editing the README.md

--- README.md
+++ README.md
@@ -13,14 +13,22 @@ https://liliia87.github.io/ProgLanguage-Suggester
 
 ## Description
 
-_This application has a few questions. It helps a user to get an output with simple and fun ansver. User should to enter his name and choose the answers._
+_This application has a few questions. It helps a user to get an output with simple and fun answer. User should to enter his name and choose the answers._
 _The application consist a survey of 5 questions_
 _There are at least 3 different language suggestions_
 
 ## Setup/Installation Requirements
 
-*_This is a webpage_
-*_All you need to do is openning the page in your brouser_
+*_Clone this git on your local computer_
+*_For clonning: use button Clone -> from repository._
+*_Copy the clone command(SSH format or HTTPS)._
+*_From a terminal on your local computer, use command "cd" to find the directory where you want to clone this repository._
+*_Type the command ($ git clone HTTPs)._
+*_If you clone successfully , a new sub-directory appears on your local computer._
+*_In the directory you will find(index.html, folder->css, folder->js)._
+*_Run the index.html (double click on it and the page should appear in web browser)._
+*_If you are willing to debug or change something in the project, the best way is to open it throught the VisuaStudio._
+
 
 ## Known Bugs
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,59 @@
-$(document).ready(function(){
-    $("form#form1").submit(function(event){
-        event.preventDefault();
-        //$("#output").html("");
-        const userName = $("#name").val();
-        const progExp = $("#purpose").val();
-        const selectOfProgLang = $("#purpose1").val();
-        const typeOfProgramming = $("input:radio[name=purpose3]:checked").val();
-        const hoursPerDay = parseInt($("#hours").val());
-        const t = $("#volume").val();
-        let message = "";
-        
+$(document).ready(function () {
+  $("form#form1").submit(function (event) {
+    event.preventDefault();
+    //$("#output").html("");
+    const userName = $("#name").val();
+    const progExp = $("#purpose").val();
+    const selectOfProgLang = $("#purpose1").val();
+    const typeOfProgramming = $("input:radio[name=purpose3]:checked").val();
+    const hoursPerDay = parseInt($("#hours").val());
+    const t = $("#volume").val();
+    let message = "";
 
 
-        // if(progExp == "Yes" && selectOfProgLang == "Yes" && typeOfProgramming == "Back end"){
-        //    $("#quote").html(". Dear " + userName + " the best language for you is C++"); 
-        // }
-        // if(progExp == "No" && selectOfProgLang == "No" && typeOfProgramming == "Front end"){
-        //     $("#quote").html(". Dear " + userName + " the best language for you is JavaScript"); 
-        //  } 
-        //  if(progExp == "No" && selectOfProgLang == "No" && typeOfProgramming == "Front end"){
-        //     $("#quote").html(". Dear " + userName + " the best language for you is JavaScript"); 
-        //  }  
-        if (progExp == "Yes"){
-            if(hoursPerDay < 3){
-                message = "Dear " + userName + ", the best language for you is Python. With "+t+"% of success";
-            } else{ 
-                if (selectOfProgLang == "Yes" && typeOfProgramming == "Back end"){
-                    message = "Dear " + userName + ", the best language for you is C++. With "+t+"% of success";
-                }
-                if (selectOfProgLang == "No" && typeOfProgramming == "Front end"){
-                    message = "Dear " + userName + ", the best language for you is Ruby. With "+t+"% of success";
-                }
-                if (selectOfProgLang == "No" && typeOfProgramming == "Back end"){
-                    message = "Dear " + userName + ", the best language for you to start is Python. With "+t+"% of success";
-                }
-                if (selectOfProgLang == "Yes" && typeOfProgramming == "Front end"){
-                    message = "Dear " + userName + ", the best language for you is JavaScript. With "+t+"% of success";
-                }
-            }
-        } else{
-            if(hoursPerDay < 3){
-                message = "Dear " + userName + ", the best language for you is Python. With "+t+"% of success";
-            } else{
-                if (selectOfProgLang == "Yes" && typeOfProgramming == "Back end"){
-                    message = "Dear " + userName + ", the best language for you is C++. With "+t+"% of success";
-                }
-                if (selectOfProgLang == "No" && typeOfProgramming == "Front end"){
-                    message = "Dear " + userName + ", the best language for you is JavaScript. With "+t+"% of success";
-                }
-                if (selectOfProgLang == "No" && typeOfProgramming == "Back end"){
-                    message = "Dear " + userName + ", the best language for you is Python. With "+t+"% of success";
-                }
-                if (selectOfProgLang == "Yes" && typeOfProgramming == "Front end"){
-                    message = "Dear " + userName + ", the best language for you is JavaScript. With "+t+"% of success";
-                }
-            }
+    if (progExp == "Yes") {
+      if (hoursPerDay < 3) {
+        message = "Dear " + userName + ", the best language for you is Python. With " + t + "% of success";
+      } else {
+        if (selectOfProgLang == "Yes" && typeOfProgramming == "Back end") {
+          message = "Dear " + userName + ", the best language for you is C++. With " + t + "% of success";
         }
+        if (selectOfProgLang == "No" && typeOfProgramming == "Front end") {
+          message = "Dear " + userName + ", the best language for you is Ruby. With " + t + "% of success";
+        }
+        if (selectOfProgLang == "No" && typeOfProgramming == "Back end") {
+          message = "Dear " + userName + ", the best language for you to start is Python. With " + t + "% of success";
+        }
+        if (selectOfProgLang == "Yes" && typeOfProgramming == "Front end") {
+          message = "Dear " + userName + ", the best language for you is JavaScript. With " + t + "% of success";
+        }
+      }
+    } else {
+      if (hoursPerDay < 3) {
+        message = "Dear " + userName + ", the best language for you is Python. With " + t + "% of success";
+      } else {
+        if (selectOfProgLang == "Yes" && typeOfProgramming == "Back end") {
+          message = "Dear " + userName + ", the best language for you is C++. With " + t + "% of success";
+        }
+        if (selectOfProgLang == "No" && typeOfProgramming == "Front end") {
+          message = "Dear " + userName + ", the best language for you is JavaScript. With " + t + "% of success";
+        }
+        if (selectOfProgLang == "No" && typeOfProgramming == "Back end") {
+          message = "Dear " + userName + ", the best language for you is Python. With " + t + "% of success";
+        }
+        if (selectOfProgLang == "Yes" && typeOfProgramming == "Front end") {
+          message = "Dear " + userName + ", the best language for you is JavaScript. With " + t + "% of success";
+        }
+      }
+    }
 
-        $("#output").fadeOut("slow", function() {
-            $("#output").html(message);
-        });
-
-        $("#output").fadeIn('slow');
-         
+    $("#output").fadeOut("slow", function () {
+      $("#output").html(message);
     });
+
+    $("#output").fadeIn('slow');
+
+  });
 
 
 
